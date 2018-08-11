667aad105ec18f168da86fa62a8a367d8f349a0e
Cédric JUNG <cedced19@gmail.com>
2018-08-11 15:10:21 +0200

    Add mail module and show results in console

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # decouverto-ftp-saver
 A tool to download files from the website a save them to ftp server.
+
+# Configuration
+
+You will have to create a `.env` file to configurate the mail service:
+```dosini
+MAIL_SERVICE=gmail
+MAIL_AUTH_USER=example@gmail.com
+MAIL_AUTH_PASS=password
+RECEIVERS=receiver@example.com,receiver2@example.com
+FTP_SERVER_PATH=/Disque dur/decouverto/
+FTP_SERVER=mafreebox.freebox.fr
+```
--- decouverto-ftp-saver.js
+++ decouverto-ftp-saver.js
@@ -1,37 +1,52 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), './.env') });
+
 const request = require('request');
 const report = require('./lib/report');
 const fs = require('fs-extra');
-const path = require('path');
 const jsftp = require("jsftp");
 const series = require('async-series');
+const colors = require('colors');
 
 const ftp = new jsftp({
-    host: 'mafreebox.freebox.fr'
+    host: process.env.FTP_SERVER
 });
 
+console.log('Téléchargement de la liste'.yellow);
 request.get('https://decouverto.fr/walks/index.json', function (err, res, body) {
     if (err) return report(err, 'Impossible de télécharger la liste des balades.');
-    ftp.put(new Buffer(body, 'utf8'), '/Disque dur/decouverto/index.json', err => {
+    console.log('Liste téléchargée'.green);
+    ftp.put(Buffer.from(body, 'utf8'), process.env.FTP_SERVER_PATH + '/index.json', err => {
         if (err) return report(err, 'Impossible de sauvegarder la liste des balades sur le serveur.');
+        console.log('Liste sauvegardée'.green);
     });
     const files = JSON.parse(body);
+
+    fs.mkdirpSync(path.join(process.cwd(), 'tmp'));
 
     const tasks = files.map(element => {
         let p = path.join(process.cwd(), 'tmp', element.id + '.zip');
         return function (callback) {
-            request.get('https://decouverto.fr/walks/' + element.id + '.zip').on('error', function(err) {
-                if (err) return callback(err);
-            })
+            request.get('https://decouverto.fr/walks/' + element.id + '.zip')
             .on('error', function (err) {
+                console.log(colors.red('Erreur lors du téléchargement de ' + element.id + '.zip'));
                 return callback(err);
             })
             .pipe(fs.createWriteStream(p))
             .on('finish', function () {
-                if (err) return callback(err);
                 fs.readFile(p, 'binary', function(err, data) {
-                    let buffer = new Buffer(data, 'binary');
-                    ftp.put(buffer, '/Disque dur/decouverto/' + element.id + '.zip', err => {
-                        if (err) return callback(err);
+                    if (err) {
+                        console.log(colors.red('Erreur lors de la lecture de ' + element.id + '.zip'));
+                        return callback(err);
+                    }
+                    console.log(colors.green('Lecture de ' + element.id + '.zip réussie'));
+                    let buffer = Buffer.from(data, 'binary');
+                    ftp.put(buffer, process.env.FTP_SERVER_PATH + '/' + element.id + '.zip', err => {
+                        if (err) {
+                            console.log(colors.red('Erreur lors de la sauvegarde de ' + element.id + '.zip'));
+                            return callback(err);
+                        }
+                        console.log(colors.green('Sauvegarde de ' + element.id + '.zip réussie'));
                         fs.remove(p, callback);
                     });
                 });
@@ -43,11 +58,11 @@ request.get('https://decouverto.fr/walks/index.json', function (err, res, body) 
         if (err) {
             return report(err, 'Une erreur est survenue lors de la sauvegarde des balades.');
         }
-        let body = 'Les balades suivantes ont été téléchargé:'
+        let body = 'Les balades suivantes ont été sauvegardées:'
         files.forEach(element => {
             body+= '\n - ' + element.title;
         });
-        report(null, 'Sauvegarde réussie', body);
+        report(null, 'Sauvegarde des balades réussie', body);
     });
 
 });
